c5b4e314e5a642056128bd7c7b349d3bce730ed7
Jose Amesquita <joseamesquita11@gmail.com>
2020-01-24 10:04:11 -0800

    1. add compute function to return the numbers 0 to the user input 2. if the user inputs alphabetical texts, it prompts the user to insert a number

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@
 function number(input) {
   var value = input.toString();
   if (value.includes("3")) {
-    return "I'm sorry, Dave. I'm afraid I can't do that";
+    return " I'm sorry, Dave. I'm afraid I can't do that";
   }
   else if (value.includes("2")) {
-    return "Beep!";
+    return " Beep!";
   }
   else if (value.includes("1")) {
-    return "Boop!";
+    return " Boop!";
   }
   else {
     return input;
@@ -17,12 +17,33 @@ function number(input) {
   console.log(value);
 }
 
+function compute(value) {
+  var numArray = [];
+  var num = value.toString();
+  var letters = /^[A-Za-z]+$/;
+  if (num.match(letters)) {
+    return "This is not a number, please enter a number";
+  }
+  else {
+    for (var i = 0; i <= num; i++) {
+      numArray.push(number(i));
+      var newArray = numArray.toString((" "));
+    }
+    console.log(numArray);
+    console.log(newArray);
+    return newArray;
+  }
+}
+
 
 $(document).ready(function () {
   $("#input").submit(function (event) {
     event.preventDefault();
     var valueExchange = number(parseInt($("input#number").val()));
+    var result = compute(parseInt($("input#number").val()));
     console.log(valueExchange);
-    $("#output").text(valueExchange);
-  })
+    console.log(result);
+    // $("#output").text(valueExchange);
+    $("#output").text(result)
+  });
 });
