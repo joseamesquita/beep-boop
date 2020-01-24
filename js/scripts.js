6c3bb2d6f6a9cea744f61cf8e6db5fea5e740298
Jose Amesquita <joseamesquita11@gmail.com>
2020-01-24 09:32:08 -0800

    1. business logic using branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
+//Business Logic
 
+function number(input) {
+  var value = input.toString();
+  if (value.includes("3")) {
+    return "I'm sorry, Dave. I'm afraid I can't do that";
+  }
+  else if (value.includes("2")) {
+    return "Beep!";
+  }
+  else if (value.includes("1")) {
+    return "Boop!";
+  }
+  else {
+    return input;
+  }
+  console.log(value);
+}
+
+
+$(document).ready(function () {
+  $("#input").submit(function (event) {
+    event.preventDefault();
+    var valueExchange = number(parseInt($("input#number").val()));
+    console.log(valueExchange);
+    $("#output").text(valueExchange);
+  })
 });
